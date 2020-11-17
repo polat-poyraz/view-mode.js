f22a55cb1480ba408b69975722a79fb17d8db1cb
polatpoyraz-1 <polat64polat34@gmail.com>
2020-11-17 23:39:00 +0300

    remove css enginer

--- view-mode.js
+++ view-mode.js
@@ -2,29 +2,6 @@ export default function viewMode(data) {
     const { name, layers, startFunctionName, animation } = data
     const Content = {}
 
-    function cssEnginer(values) {
-        let cssCodes = ''
-        
-        function caseMutation(item){
-            let result = ''
-
-            for (let i in item) {
-                if (item[i] === item[i].toUpperCase())
-                    result += `-${item[i].toLowerCase()}`
-                else
-                    result += item[i]
-            }
-
-            return result
-        }
-
-        for (const prop in values){
-            cssCodes += `${caseMutation(prop)}: ${values[prop]}; `
-        }
-        
-        return cssCodes
-    }
-
     function animationEnginer() {
         return `${animation.mode ? `transition: ${animation.time};` : null }`
     }
@@ -37,7 +14,13 @@ export default function viewMode(data) {
                 const All = document.querySelectorAll(name)
 
                 for (let i = 0; i < All.length; i++){
-                    All[i].style = `${animationEnginer()} ${cssEnginer(css)}`
+                    for (const prop in css) {
+                        const key = prop
+                        const cssValue = css[prop]
+
+                        All[i].style[key] = cssValue
+                        All[i].style = animationEnginer()
+                    }
                 }
             }
         }, window.onload)
